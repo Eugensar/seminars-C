2306b224314c6592d63e8b17696a2720369060ac
Eugensar <eugensar@gmail.com>
2022-09-25 19:02:43 +0400

    task 19 - OK

---
+++ task19/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */ 
+
+
+int getСhangeling(int number)
+{
+int chang = 0;
+while (number > 0)
+{
+     int numb = number % 10;
+     chang = chang * 10 + numb;
+     number = number / 10;
+}
+return chang;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = getСhangeling(number);
+if (number == number1)
+{
+    Console.WriteLine($"число {number} является палиндромом");
+
+}
+else
+    Console.WriteLine($"число {number} НЕ является палиндромом");
+
+
